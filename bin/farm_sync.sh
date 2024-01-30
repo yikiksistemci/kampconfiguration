00cf3c54d035cd6a1b8c9d7a93f8dee45bf25f8e
Yunus Yasar <yunus.yasar@pm.me>
2024-01-30 16:20:10 +0300

    fix

--- bin/farm_sync.sh
+++ bin/farm_sync.sh
@@ -6,7 +6,7 @@ cd /tmp/ && rm -rf kampconfiguration
 cd /tmp/ && git clone https://github.com/yikiksistemci/kampconfiguration.git \
 && cd /tmp/kampconfiguration && git checkout main
 
-mkdir -p ${BASE_CONF_DIR} || echo "konfig klasoru olusturulamadi."; exit 1
+mkdir -p ${BASE_CONF_DIR} || echo "konfig klasoru olusturulamadi."
 
 
-rsync -aP /tmp/kampconfiguration --exclude=".git" ${BASE_CONF_DIR}
+rsync -aP /tmp/kampconfiguration/* --exclude=".git" ${BASE_CONF_DIR}
